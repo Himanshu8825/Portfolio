27695fadfa542d00849a19fb9267fbe67aa5fea5
Suraj Verma <121680980+Himanshu8825@users.noreply.github.com>
2024-10-06 22:43:33 +0530

    Add files via upload
    assests

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import logo4 from "./images/ProfilePic.jpg";
+import bannerImg from "./images/bannerImg.png";
+import bannerImg2 from "./images/bannerImg2.png";
+import contactImg from './images/contact/contactImg.png';
+
+export { bannerImg, bannerImg2, logo4 , contactImg };
